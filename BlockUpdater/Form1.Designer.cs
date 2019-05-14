9fb6c4b7df8fce2e1a29a3e2cf79ae15f600b26b
lugDroid <diazbastos@gmail.com>
2019-05-14 12:55:35 +0200

    Initial commit

---
+++ BlockUpdater/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace CopyBlocks
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_OpenProject = new System.Windows.Forms.Button();
+            this.statusBox = new System.Windows.Forms.TextBox();
+            this.btn_Close = new System.Windows.Forms.Button();
+            this.devicesCheckList = new System.Windows.Forms.CheckedListBox();
+            this.btn_ConnectProject = new System.Windows.Forms.Button();
+            this.btn_ReadProject = new System.Windows.Forms.Button();
+            this.btn_CheckSelection = new System.Windows.Forms.Button();
+            this.projectLibraryCheckList = new System.Windows.Forms.CheckedListBox();
+            this.resultsTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_OpenProject
+            // 
+            this.btn_OpenProject.Location = new System.Drawing.Point(12, 12);
+            this.btn_OpenProject.Name = "btn_OpenProject";
+            this.btn_OpenProject.Size = new System.Drawing.Size(113, 47);
+            this.btn_OpenProject.TabIndex = 0;
+            this.btn_OpenProject.Text = "Open Project";
+            this.btn_OpenProject.UseVisualStyleBackColor = true;
+            this.btn_OpenProject.Click += new System.EventHandler(this.btn_OpenProject_Click);
+            // 
+            // statusBox
+            // 
+            this.statusBox.Location = new System.Drawing.Point(12, 517);
+            this.statusBox.Multiline = true;
+            this.statusBox.Name = "statusBox";
+            this.statusBox.Size = new System.Drawing.Size(819, 45);
+            this.statusBox.TabIndex = 1;
+            // 
+            // btn_Close
+            // 
+            this.btn_Close.Location = new System.Drawing.Point(718, 12);
+            this.btn_Close.Name = "btn_Close";
+            this.btn_Close.Size = new System.Drawing.Size(113, 47);
+            this.btn_Close.TabIndex = 2;
+            this.btn_Close.Text = "Close";
+            this.btn_Close.UseVisualStyleBackColor = true;
+            this.btn_Close.Click += new System.EventHandler(this.btn_Close_Click);
+            // 
+            // devicesCheckList
+            // 
+            this.devicesCheckList.FormattingEnabled = true;
+            this.devicesCheckList.Location = new System.Drawing.Point(289, 72);
+            this.devicesCheckList.Name = "devicesCheckList";
+            this.devicesCheckList.Size = new System.Drawing.Size(268, 439);
+            this.devicesCheckList.TabIndex = 3;
+            // 
+            // btn_ConnectProject
+            // 
+            this.btn_ConnectProject.Location = new System.Drawing.Point(131, 12);
+            this.btn_ConnectProject.Name = "btn_ConnectProject";
+            this.btn_ConnectProject.Size = new System.Drawing.Size(113, 47);
+            this.btn_ConnectProject.TabIndex = 4;
+            this.btn_ConnectProject.Text = "Connect Project";
+            this.btn_ConnectProject.UseVisualStyleBackColor = true;
+            this.btn_ConnectProject.Click += new System.EventHandler(this.btn_ConnectProject_Click);
+            // 
+            // btn_ReadProject
+            // 
+            this.btn_ReadProject.Location = new System.Drawing.Point(250, 12);
+            this.btn_ReadProject.Name = "btn_ReadProject";
+            this.btn_ReadProject.Size = new System.Drawing.Size(113, 47);
+            this.btn_ReadProject.TabIndex = 5;
+            this.btn_ReadProject.Text = "Read Project";
+            this.btn_ReadProject.UseVisualStyleBackColor = true;
+            this.btn_ReadProject.Click += new System.EventHandler(this.btn_ReadProject_Click);
+            // 
+            // btn_CheckSelection
+            // 
+            this.btn_CheckSelection.Location = new System.Drawing.Point(369, 12);
+            this.btn_CheckSelection.Name = "btn_CheckSelection";
+            this.btn_CheckSelection.Size = new System.Drawing.Size(113, 47);
+            this.btn_CheckSelection.TabIndex = 6;
+            this.btn_CheckSelection.Text = "Check Selection";
+            this.btn_CheckSelection.UseVisualStyleBackColor = true;
+            this.btn_CheckSelection.Click += new System.EventHandler(this.btn_CheckSelection_Click);
+            // 
+            // projectLibraryCheckList
+            // 
+            this.projectLibraryCheckList.FormattingEnabled = true;
+            this.projectLibraryCheckList.Location = new System.Drawing.Point(12, 72);
+            this.projectLibraryCheckList.Name = "projectLibraryCheckList";
+            this.projectLibraryCheckList.Size = new System.Drawing.Size(268, 439);
+            this.projectLibraryCheckList.TabIndex = 8;
+            // 
+            // resultsTextBox
+            // 
+            this.resultsTextBox.Location = new System.Drawing.Point(564, 72);
+            this.resultsTextBox.Multiline = true;
+            this.resultsTextBox.Name = "resultsTextBox";
+            this.resultsTextBox.ReadOnly = true;
+            this.resultsTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.resultsTextBox.Size = new System.Drawing.Size(267, 439);
+            this.resultsTextBox.TabIndex = 9;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(843, 574);
+            this.Controls.Add(this.resultsTextBox);
+            this.Controls.Add(this.projectLibraryCheckList);
+            this.Controls.Add(this.btn_CheckSelection);
+            this.Controls.Add(this.btn_ReadProject);
+            this.Controls.Add(this.btn_ConnectProject);
+            this.Controls.Add(this.devicesCheckList);
+            this.Controls.Add(this.btn_Close);
+            this.Controls.Add(this.statusBox);
+            this.Controls.Add(this.btn_OpenProject);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_OpenProject;
+        private System.Windows.Forms.TextBox statusBox;
+        private System.Windows.Forms.Button btn_Close;
+        private System.Windows.Forms.CheckedListBox devicesCheckList;
+        private System.Windows.Forms.Button btn_ConnectProject;
+        private System.Windows.Forms.Button btn_ReadProject;
+        private System.Windows.Forms.Button btn_CheckSelection;
+        private System.Windows.Forms.CheckedListBox projectLibraryCheckList;
+        private System.Windows.Forms.TextBox resultsTextBox;
+    }
+}
+
---
+++ BlockUpdater/Form1.cs
@@ -0,0 +1,254 @@
+﻿using Microsoft.Win32;
+using Siemens.Engineering;
+using Siemens.Engineering.HW.Features;
+using Siemens.Engineering.Library;
+using Siemens.Engineering.Library.MasterCopies;
+using Siemens.Engineering.Library.Types;
+using Siemens.Engineering.SW;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CopyBlocks
+{
+    public partial class MainForm : Form
+    {
+        public TiaPortal MyTiaPortal
+        {
+            get; set;
+        }
+        public Project MyProject
+        {
+            get; set;
+        }
+
+        public static TiaPortalProcess _tiaProcess;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            AppDomain CurrentDomain = AppDomain.CurrentDomain;
+            CurrentDomain.AssemblyResolve += new ResolveEventHandler(MyResolver);
+        }
+
+        private static Assembly MyResolver(object sender, ResolveEventArgs args)
+        {
+            int index = args.Name.IndexOf(',');
+            if (index == -1)
+            {
+                return null;
+            }
+            string name = args.Name.Substring(0, index);
+
+            RegistryKey filePathReg = Registry.LocalMachine.OpenSubKey(
+                "SOFTWARE\\Siemens\\Automation\\Openness\\15.0\\PublicAPI\\15.0.0.0");
+
+            object oRegKeyValue = filePathReg?.GetValue(name);
+            if (oRegKeyValue != null)
+            {
+                string filePath = oRegKeyValue.ToString();
+
+                string path = filePath;
+                string fullPath = Path.GetFullPath(path);
+                if (File.Exists(fullPath))
+                {
+                    return Assembly.LoadFrom(fullPath);
+                }
+            }
+
+            return null;
+        }
+
+        // FUNCTIONS
+        private void StartTIA(object sender, EventArgs e)
+        {
+            MyTiaPortal = new TiaPortal(TiaPortalMode.WithoutUserInterface);
+        }
+
+        // read project library master copies folder and adds all elements to given list box
+        private void readProjectLibrary(CheckedListBox projectListBox, MasterCopyFolder mCopiesFolder, string path)
+        {
+            if (mCopiesFolder == null)
+                throw new ArgumentNullException(nameof(mCopiesFolder), "Parameter is null");
+
+            // Disable list box updating before adding new elements
+            projectListBox.BeginUpdate();
+
+            // Add new elements
+            path = path + mCopiesFolder.Name + "/";
+
+            foreach (var mCopy in mCopiesFolder.MasterCopies)
+            {
+                projectListBox.Items.Add(path + mCopy.Name);
+            }
+
+            foreach (var subfolder in mCopiesFolder.Folders)
+            {
+                this.readProjectLibrary(projectListBox, subfolder, path);
+            }
+
+            // Enable list box updating again
+            projectListBox.EndUpdate();
+        }
+
+        // BUTTON EVENTS
+        private void btn_OpenProject_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileSearch = new OpenFileDialog();
+
+            fileSearch.Filter = "*.ap15|*.ap15";
+            fileSearch.RestoreDirectory = true;
+            fileSearch.ShowDialog();
+
+            string ProjectPath = fileSearch.FileName.ToString();
+
+            this.StartTIA(sender, e);
+
+            if (string.IsNullOrEmpty(ProjectPath) == false)
+            {
+                OpenProject(ProjectPath);
+            }
+        }
+
+        private void OpenProject(string ProjectPath)
+        {
+            try
+            {
+                MyProject = MyTiaPortal.Projects.Open(new FileInfo(ProjectPath));
+                statusBox.Text = "Project " + ProjectPath + " opened";
+            }
+            catch (Exception ex)
+            {
+                statusBox.Text = "Error while opening project: " + ProjectPath + " - " + ex.Message;
+            }
+        }
+
+        private void btn_Close_Click(object sender, EventArgs e)
+        {
+            MyTiaPortal.Dispose();
+            statusBox.Text = "TIA Portal disposed";
+        }
+
+        // connect to existing project
+        private void btn_ConnectProject_Click(object sender, EventArgs e)
+        {
+            
+            IList<TiaPortalProcess> processes = TiaPortal.GetProcesses();
+            statusBox.Text = "Attempting to connect..." + processes.Count + " processes found";
+
+            switch (processes.Count)
+            {
+                case 1:
+                    _tiaProcess = processes[0];
+                    MyTiaPortal = _tiaProcess.Attach();
+
+                    if (MyTiaPortal.Projects.Count <= 0)
+                    {
+                        statusBox.Text = "No TIA Portal Project was found!";
+                        return;
+                    }
+                    statusBox.Text = "Connected to running instance of TIA Portal";
+                    MyProject = MyTiaPortal.Projects[0];
+                    break;
+                case 0:
+                    statusBox.Text = "No running instance of TIA Portal was found!";
+                    return;
+                default:
+                    statusBox.Text = "More than one running instance of TIA Portal was found!";
+                    return;
+            }
+        }
+
+        // Read project and display devices and library items on check lists
+        private void btn_ReadProject_Click(object sender, EventArgs e)
+        {
+            // Read project Devices and add them to check list
+            ArrayList deviceList = new ArrayList();
+
+            foreach (var device in MyProject.Devices)
+            {
+                deviceList.Add(device.TypeIdentifier + " - " + device.Name.ToString());   
+            }
+
+            deviceList.Sort();
+
+            foreach (var device in deviceList)
+            {
+                devicesCheckList.Items.Add(device);
+            }
+
+            // Read project library
+            this.readProjectLibrary(projectLibraryCheckList, MyProject.ProjectLibrary.MasterCopyFolder, "");
+        }
+
+        // Check items selected
+        // TODO check project library items
+        private void btn_CheckSelection_Click(object sender, EventArgs e)
+        {
+            // Determine if there are any devices checked.  
+            if (devicesCheckList.CheckedItems.Count != 0)
+            {
+                // If so loop through all devices checking if they have been selected
+                foreach (var device in MyProject.Devices)
+                {
+                    if (devicesCheckList.CheckedItems.Contains(device.Name))
+                    {
+                        foreach (var deviceItem in device.DeviceItems)
+                        {
+                            SoftwareContainer softwareContainer = ((IEngineeringServiceProvider)deviceItem).GetService<SoftwareContainer>();
+                            if (softwareContainer != null)
+                            {
+                                PlcSoftware software = softwareContainer.Software as PlcSoftware;
+
+                                // iterate over project library items and check if they have been selected
+                                // comparing against checked list
+                                //foreach (var masterCopy in )
+
+                                //foreach (string blockPath in projectLibraryCheckList.CheckedItems)
+
+                                // copy selected blocks from project library
+                                //foreach (var softwareBlock in projectLibrarySelected)
+                                //{
+                                //    software.BlockGroup.Blocks.CreateFrom(softwareBlock);
+                                //}
+                                
+
+                                //string str = "";
+                                
+                                //foreach (var group in software.BlockGroup.Groups)
+                                //{
+                                    //str = str + group.Name.ToString() + Environment.NewLine;
+                                    //foreach (var block in group.Blocks)
+                                    //{
+                                    //    str = str + group.Name.ToString() + " - " + block.Name.ToString() + Environment.NewLine;
+                                    //}
+                                //}
+
+                                //resultsTextBox.Text = str;
+                            }
+                        }
+                    }
+                    
+                }
+                // If so, loop through all checked items and print results.  
+                //string s = "";
+                //for (int x = 0; x < devicesCheckList.CheckedItems.Count; x++)
+                //{
+                //    s = s + "Checked Item " + (x + 1).ToString() + " = " + devicesCheckList.CheckedItems[x].ToString() + "\n";
+                //}
+                //MessageBox.Show(s);
+            }
+
+            
+        }
+    }
+}
