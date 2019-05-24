7eb53963e7e6864fa85c28cae7552a111622fa08
lugDroid <diazbastos@gmail.com>
2019-05-24 17:41:29 +0200

    Alert icon fixed

--- BlockUpdater/AlertForm.Designer.cs
+++ BlockUpdater/AlertForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AlertForm));
             this.btnOK = new System.Windows.Forms.Button();
             this.alertTextBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
@@ -63,6 +64,7 @@
             this.Controls.Add(this.btnOK);
             this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
